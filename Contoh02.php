f73b347a14ea45b9e346c322e15f630058ea1c1a
Andika36 <setiawanandika264@gmail.com>
2019-10-30 20:00:47 +0700

    ini commit pertama

---
+++ Contoh02.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title>Contoh 02</title>
+</head>
+<body text=blue bgcolor=#FF0009>
+<h1 align=center>Andika Setiawan</h1>
+<br>
+<hr size=5 width=50% align=center>
+</body>
+</html>
